db4a191851626aa0695b76af6bf8143b81494651
colombelli <xcolombelli@gmail.com>
2019-10-29 01:45:21 -0300

    Genetic Algorithm: first try

--- controller1/controller.py
+++ controller1/controller.py
@@ -226,6 +226,177 @@ class Controller(controller_template.Controller):
         return  # argsort dos melhores candidatos e pega o parametro do melhor
 
 
+
+    def genetic_algorithm(self, weights):
+
+            population_size = 200
+            elitism = 0.1
+            roulette = 0.1
+            mutation_rate = 0.2
+            max_generations = 500
+            max_same_best = 10
+
+            population = self.generate_popuation(len(weights), population_size)
+            fitness = self.compute_fitness(population)
+
+            generation = 1
+            same_best = 0
+
+            best_idx = np.argmax(fitness)
+            best_individual_prev = population[best_idx]
+
+            while generation <= max_generations:
+                
+                print("\n\nGeneration:", generation)
+                print("Best score:", fitness[best_idx])
+                print("\n\n")
+
+                population = self.select_population(population, fitness, elitism, roulette)
+                population = self.cross_population(population, population_size)
+                population = self.mutate_population(population, mutation_rate)
+                fitness = self.compute_fitness(population)
+                generation += 1
+
+                best_idx = np.argmax(fitness)
+                best_individual = population[best_idx]
+
+                if (np.array_equal(best_individual, best_individual_prev)):
+                    same_best += 1
+                    if (same_best >= max_same_best):
+                        print("Same individual found", same_best,
+                            "times. Learning algorithm stopped.")
+                        return population[np.argmax(fitness)]
+
+                else:
+                    same_best = 0
+
+                best_individual_prev = best_individual
+
+            print("Max generations reached. Learning algorithm stopped.")
+            return population[np.argmax(fitness)]
+
+
+    # Input parameters: list size of the current weights; number of individuals to be generated
+    # Output returned: new set of weights
+    def generate_popuation(self, weights_len, population_size):
+
+        population = []
+
+        for i in range(population_size):
+            population.append(
+                np.random.uniform(low=-1.0, high=1.0, size=weights_len)
+            )
+        return population
+
+
+    # Input parameters: list of individual solutions
+    # Output returned: list of fitness score for each individual
+    def compute_fitness(self, population):
+
+        fitness = []
+
+        for individual in population:
+            fitness.append(self.run_episode(individual))
+
+        return fitness
+
+
+    # Input parameters: list of individual solutions; fitness; fraction to keep by elitism; fraction to keep by roulette
+    # Output returned: selected individuals
+    def select_population(self, population, fitness, elitism, roulette):
+
+        num_from_elite = round(elitism * len(population))
+        num_from_roulette = round(roulette * len(population))
+        
+        fitness = np.array(fitness)
+        population = np.array(population)
+
+        # Get the index of the N greatest scores
+        elite_idx = np.argpartition(fitness, -num_from_elite)[-num_from_elite:]
+        elite = population[elite_idx]
+        
+        # Delete the already selected individuals by the elitism method
+        population = np.delete(population, elite_idx, axis=0)
+        fitness = np.delete(fitness, elite_idx, axis=0)
+
+        # Select the next fraction by the roulette method
+        drawn_from_roulette = []
+        s = np.sum(fitness)
+
+        for _ in range(num_from_roulette):
+            r = np.random.randint(0,s)
+            t = 0
+            for i, score in enumerate(fitness):
+                t = t + score
+                if (t >= r):
+                    drawn_from_roulette.append(population[i])   # get rouletted individual
+                    population = np.delete(population, i, axis=0)   # can't be chosen more than once
+                    fitness = np.delete(fitness, i, axis=0)   # can't be chosen more than once
+                    break
+
+        drawn_from_roulette = np.array(drawn_from_roulette)
+        selected_population = np.append(elite, drawn_from_roulette, axis=0)
+
+        return selected_population
+
+
+    # Crossover method used: uniform crossover with random crossover mask
+    # Input parameters: list of individual solutions; list of maximum individuals
+    # Output returned: new population
+    def cross_population(self, population, population_size):
+
+        missing_population = []
+        num_missing_individuals = population_size - len(population)
+
+        mask = np.random.randint(0, 2, size=population.shape[1])
+        # mask example for a problem with 5 weights [0,1,1,0,1] 
+        # Note that, here, each weight is the gene of the chromossome (instead of bits)
+
+        for _ in range(num_missing_individuals):
+            dad1_idx = np.random.randint(0, len(population))
+            dad2_idx = np.random.randint(0, len(population))
+            dad1 = population[dad1_idx]
+            dad2 = population[dad2_idx]
+            son = []
+
+            for i, gene in enumerate(mask):
+                if gene == 0:
+                    son.append(dad1[i])
+                else:
+                    son.append(dad2[i])
+            
+            son = np.array(son)
+            missing_population.append(son)
+
+        missing_population = np.array(missing_population)
+        new_population = np.append(population, missing_population, axis=0)
+        return new_population
+
+
+    # Input parameters: list of individual solutions;
+    # Output returned: new population
+    def mutate_population(self, population, mutation_rate, perturbation_range=0.1):
+
+        mutation = mutation_rate*100
+        for individual in population:
+            rand = np.random.randint(0, 101)
+            
+            if self.must_mutate(rand, mutation):
+                # Mutate a random number of times, a random number of genes
+                for _ in range(np.random.randint(0, len(individual))):
+                    mutation_gene = np.random.randint(0, len(individual))
+                    perturbation = np.random.uniform(low=-perturbation_range, high=perturbation_range)
+                    individual[mutation_gene] += perturbation
+
+        return population
+
+
+    def must_mutate(self, rand, mutation):
+        return rand <= mutation
+
+
+
+
     # vou deixar essa Coisa aqui ate achar um jeito pratico de usar
     @property
     def distLeft(self):
